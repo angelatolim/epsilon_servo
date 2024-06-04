99cf2e9c256bde4a8ebedeafd744497721363a15
angelaliu792 <angela.liu792@gmail.com>
2024-06-04 16:19:02 +1000

    ownerlist for stats created

---
+++ client/components/owner_list.js
@@ -0,0 +1,40 @@
+import * as mapApi from '../map_api.js'
+
+// set elems to be used
+const ownerList = document.querySelector('.owner-list')
+
+mapApi.fetchOwnerStats()
+  .then(ownerStats => {
+    ownerStats.forEach( ownerStat => {
+      let ownerName = ownerStat.owner
+      let ownerCount = ownerStat.count 
+      let ownerDiv = document.createElement('div')
+      let nameDiv = document.createElement('div')
+      let countDiv = document.createElement('div')
+      nameDiv.textContent = ownerName
+      countDiv.textContent = ownerCount
+      ownerDiv.className = 'owner-div'
+
+      console.log(ownerCount);
+
+      ownerDiv.appendChild(nameDiv)
+      ownerDiv.appendChild(countDiv)
+      ownerDiv.style.display = 'flex'
+      ownerDiv.style.flexDirection = 'row'
+      ownerDiv.style.justifyContent = 'space-between'
+
+      ownerList.appendChild(ownerDiv)
+
+    })
+  })
+
+mapApi.fetchTen()
+  .then(stations => {
+    stations.forEach( station => {
+      let stationElem = createStationElem(station)
+      ownerList.appendChild(stationElem)
+    })
+})
+
+
+// functions
