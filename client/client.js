ec7d71d8be81e46dbd24785f5c9cfa3c5bdf32ce
bernadinedao <bernadine.dao@gmail.com>
2024-06-04 11:23:35 +1000

    map to js

--- client/client.js
+++ client/client.js
@@ -1,2 +1,22 @@
+let map;
 
+async function initMap() {
+  const position = { lat: -25.344, lng: 131.031 };
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
+  map = new Map(document.getElementById("map"), {
+    zoom: 13,
+    minZoom: 8,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Marker",
+  });
+}
+
+initMap();
