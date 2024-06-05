e37a6f1a14e5a05f1466a1c6503d227a373eb86a
Eric Stuart-Jones <stuartjones002@gmail.com>
2024-06-05 22:55:43 +1000

    added error handling to input

--- client/components/postcode.js
+++ client/components/postcode.js
@@ -1,26 +1,29 @@
 import * as suburbApi from "../suburb_api.js";
 
-const postcodeSearchForm = document.querySelector('.postcode-search-form');
-const postcodeSearchInput = document.querySelector('.postcode-search-input');
-const suburbResults = document.querySelector('.suburb-results');
-const suburbTotal = document.querySelector('.suburb-total');
+const postcodeSearchForm = document.querySelector(".postcode-search-form");
+const postcodeSearchInput = document.querySelector(".postcode-search-input");
+const suburbResults = document.querySelector(".suburb-results");
+const suburbTotal = document.querySelector(".suburb-total");
 
-postcodeSearchForm.addEventListener('submit', handlePostcodeSubmit);
+postcodeSearchForm.addEventListener("submit", handlePostcodeSubmit);
 
-function handlePostcodeSubmit (event) {
-    event.preventDefault();
-    suburbResults.textContent = "";
-    const postcode = postcodeSearchInput.value;
+function handlePostcodeSubmit(event) {
+  event.preventDefault();
+  suburbResults.textContent = "";
+  const postcode = postcodeSearchInput.value;
 
-    suburbApi.fetchSuburbs(postcode)
-        .then(suburbs => {
-            suburbs.forEach(suburb => {
-                const name = suburb.name;
-                const button = document.createElement('button');
-                button.textContent = name;
-                suburbResults.appendChild(button);
-            })
-            const totalSuburbs = suburbs.length;
-            suburbTotal.textContent = `${totalSuburbs} results`;
-        })
+  if (Number.isNaN(Number(postcode)) || postcode.length < 3) {
+    suburbTotal.textContent = `0 results`;
+  } else {
+    suburbApi.fetchSuburbs(postcode).then((suburbs) => {
+      suburbs.forEach((suburb) => {
+        const name = suburb.name;
+        const button = document.createElement("button");
+        button.textContent = name;
+        suburbResults.appendChild(button);
+      });
+      const totalSuburbs = suburbs.length;
+      suburbTotal.textContent = `${totalSuburbs} results`;
+    });
+  }
 }
