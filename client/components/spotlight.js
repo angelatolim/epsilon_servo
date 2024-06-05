61799a3b8518d928b6b229d342d35298a437e7fd
bernadinedao <bernadine.dao@gmail.com>
2024-06-05 18:19:51 +1000

    infowindow

--- client/components/spotlight.js
+++ client/components/spotlight.js
@@ -1,5 +1,6 @@
 import * as MapApi from "../map_api.js";
 import { getMap } from "./map.js";
+import * as Utils from "../utils.js";
 
 const spotlightDiv = document.querySelector(".spotlight-content");
 const refreshBtn = document.querySelector(".refresh-btn");
@@ -23,12 +24,72 @@ async function renderSpotlight() {
   nameLink.className = "name-link";
   nameLink.addEventListener("click", handleLink);
 
-  function handleLink(event) {
+  async function handleLink(event) {
     const latLng = {
       lat: data.latitude,
       lng: data.longitude,
     };
     getMap().setCenter(latLng);
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+    const lat = Number(data.latitude);
+    const lng = Number(data.longitude);
+
+    const markerImg = document.createElement("img");
+    markerImg.className = "marker";
+
+    switch (data.owner) {
+      case "Caltex":
+        markerImg.src = "../images/caltex.png";
+        break;
+      case "Shell":
+        markerImg.src = "../images/shell.png";
+        break;
+      case "7-Eleven Pty Ltd":
+        markerImg.src = "../images/7-eleven.png";
+        break;
+      case "Ampol":
+        markerImg.src = "../images/ampol.png";
+        break;
+      case "BP":
+        markerImg.src = "../images/bp.png";
+        break;
+      case "United":
+        markerImg.src = "../images/united.png";
+        break;
+      default:
+        markerImg.src = "../images/fuel.png";
+    }
+
+    const marker = new AdvancedMarkerElement({
+      map: getMap(),
+      position: latLng,
+      title: data.name,
+      content: markerImg,
+    });
+
+    const name = data.name;
+    const address = data.address;
+    const owner = data.owner;
+
+    const contentString = `<div id="content">
+      <h1>${name}</h1>
+      <p>${address}</p>
+      <p>${owner}</p>
+      <p>${lat.toFixed(6)}</p>
+      <p>${lng.toFixed(6)}</p>
+      <button>Save</button>
+    </div>
+    `;
+    const infowindow = new google.maps.InfoWindow({
+      content: contentString,
+      // ariaLabel: "NAME"
+    });
+
+    infowindow.open({
+      anchor: marker,
+      map: getMap(),
+    });
   }
 
   const addressDiv = document.createElement("div");
