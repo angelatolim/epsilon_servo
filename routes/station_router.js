40bbd66bd5ea5603b1f0a6e8b410db0c68afb46c
georgia-a-scott <georgiaxscott@gmail.com>
2024-06-05 12:19:20 +1000

    updated schema and reseeded database

--- routes/station_router.js
+++ routes/station_router.js
@@ -15,4 +15,19 @@ router.get('/api/stations/random', (req, res) => {
   Station.findRandom().then((data) => res.status(200).json(data))
 })
 
+router.get('/api/stations/bounds', (req, res) => {
+
+  let topLeft = {
+            lat: -31.810536,
+            long: 115.664177
+  }
+
+  let bottomRight = {
+            lat: -32.171187,
+            long: 116.114275
+  }
+
+  Station.findByBounds(topLeft, bottomRight).then((data) => res.status(200).json(data))
+})
+
 module.exports = router;
